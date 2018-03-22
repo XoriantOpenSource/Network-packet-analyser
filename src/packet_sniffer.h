1be2253d6d22cafecb4e4d57b12ea1e9e9612dc2
root <root@localhost.localdomain>
2018-03-22 05:18:05 -0400

    Modified and added function prototypes

--- src/packet_sniffer.h
+++ src/packet_sniffer.h
@@ -15,10 +15,11 @@
 #include <netinet/tcp.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#define MAX_BUFFER_SIZE 65535
 
 void process_packet(char* , int);
-void print_ip_datagram(char*, int ); 
+void print_ip_header(char*, int ); 
 void print_tcp_packet(char*, int ); 
-void print_udp_datagram(char*, int ); 
+void print_udp_packet(char*, int ); 
 void print_icmp_packet(char*, int ); 
- 
+void print_data(char*, int ); 
